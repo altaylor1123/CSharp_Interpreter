47702253c871d86994177a123f3e8330404a27c4
Alex Taylor <altaylor1123@gmail.com>
2023-06-21 14:52:25 -0500

    adds REPL console

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Interpreter;
+
+Console.WriteLine("Hello {0}! This is the Monkey programming language!", Environment.UserName);
+Console.WriteLine("Feel free to type in commands");
+
+string? input = Console.ReadLine() ?? throw new ArgumentException("Input is null");
+
+Lexer lex = Lexer.CreateFromInput(input);
+
+Token tok = lex.NextToken();
+while (tok.Type != new TokenType(TokenTypes.EOF))
+{
+	Console.WriteLine("Type: {0} Literal: {1}", tok.Type, tok.Literal);
+	tok = lex.NextToken();
+}
